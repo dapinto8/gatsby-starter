f6d9968cd0d7a07577341b06bbef224e0b1e9513
David Pinto <davidpinto999@gmail.com>
2021-01-04 10:46:18 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,81 @@
+const env = process.env.NODE_ENV;
+
+require('dotenv').config({
+  path: `.env.${env}`
+})
+
+const isProduction = env === 'production';
+
 module.exports = {
   siteMetadata: {
-    title: "gatsby-starter",
+    siteUrl: 'https://www.example.com',
+    title: 'gatsby-starter',
+    description: 'Gatsby starter project',
+    author: '@dapinto8',
   },
   plugins: [
-    "gatsby-plugin-sass",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-sass',
       options: {
-        trackingId: "",
+        data: `@import '${__dirname}/src/styles/global.scss';`,
+        cssLoaderOptions: {
+          importLoaders: 1,
+          sourceMap:  !isProduction,
+          localIdentName: isProduction ? '[hash:base64:5]' : '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
+    /*{
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_TRACKING_ID || null,
+        head: true
+      }
+    },*/
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaultQuality: 100
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-react-helmet-canonical-urls',
       options: {
-        icon: "src/images/icon.png",
-      },
+        siteUrl: 'https://www.example.com'
+      }
     },
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'gatsby-starter',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#eC7f00',
+        theme_color: '#eC7f00',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png'
+      }
+    },
+    'gatsby-plugin-mdx',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
       },
-      __key: "images",
+      __key: 'images'
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/'
       },
-      __key: "pages",
+      __key: 'pages'
     },
   ],
 };
---
+++ gatsby-node.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [
+        path.resolve(__dirname, 'src'), 'node_modules',
+      ]
+    },
+  })
+};
+
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const pages = [
+    {
+      id: 1,
+      template: 'home/index.jsx',
+      locale: 'es',
+      pagePath: '/'
+    },
+    {
+      id: 2,
+      template: 'home/index.jsx',
+      locale: 'en',
+      pagePath: '/'
+    }
+  ];
+
+  pages.map(({ id, template, locale, pagePath }) => {
+    const prefix = locale === 'en' ? '' : `/${locale}`;
+    return createPage({
+      path: `${prefix}${pagePath}`,
+      component: path.resolve(`./src/templates/${template}`),
+      context: {
+        id,
+        locale
+      }
+    });
+  });
+};
